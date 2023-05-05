31276b810794d968fdea51fe97e9ac3da9972f6d
Faruqoloyede <oloyedefaru2@gmail.com>
2023-05-05 11:34:53 -0700

    added a testimonial section

--- app.js
+++ app.js
@@ -13,31 +13,50 @@ window.onscroll = ()=>{
     nav.classList.remove("show");
 };
 
-// Form.addEventListener("click", (e)=>{
-//     e.preventDefault();
-//     Email.send({
-//         Host : "smtp.elasticemail.com",
-//         Username : "thesourcenaturals@gmail.com",
-//         Password : "@Ayanfemi1",
-//         To : 'opeyemiayandokun180@gmail.com',
-//         From : document.getElementById("email"),
-//         Subject : "This is the subject",
-//         Body : "And this is the body"
-//     }).then(
-//       message => alert(message)
-//     );
-// });
 
-// function sendEmail(){
-//     Email.send({
-//         Host : "smtp.elasticemail.com",
-//         Username : "thesourcenaturals@gmail.com",
-//         Password : "@Ayanfemi1",
-//         To : 'opeyemiayandokun180@gmail.com',
-//         From : document.getElementById("email"),
-//         Subject : "This is the subject",
-//         Body : "And this is the body"
-//     }).then(
-//       message => alert(message)
-//     );
-// }
+//TESTIMONIAL
+const testimonialsContainer = document.querySelector('.testimonial-container')
+const testimonial = document.querySelector('.testimonial')
+const user = document.querySelector(".user")
+
+const testimonials = [
+  {
+    text:
+      "Overall, I would highly recommend this medical provider to anyone seeking high-quality medical care. They truly went above and beyond to provide exceptional service, and I am grateful to have had the opportunity to receive care from such a wonderful team.",
+    name: "_happy client"
+  },
+  {
+    text:
+      "I can really feel there's a lot of improvement in my system following your plan along with the treatment I'm taking.",
+    name: "_happy client"
+  },
+  {
+    text:
+      "Overall, I would highly recommend this medical provider to anyone seeking high-quality medical care. They truly went above and beyond to provide exceptional service, and I am grateful to have had the opportunity to receive care from such a wonderful team.",
+    name: "_happy client"
+  },
+  {
+    text:
+    "Thanks to the treatment I received, my pain has been significantly reduced, and I am now able to lead a much more active and fulfilling life. I no longer feel limited by my condition and am able to enjoy activities that were once impossible for me.",
+    name: "_happy client"
+  },
+]
+
+let idx = 1
+
+function updateTestimonial() {
+  const { text, name} = testimonials[idx]
+
+  testimonial.innerHTML = text
+  user.innerHTML = name
+
+  idx++
+
+  if (idx > testimonials.length - 1) {
+    idx = 0
+  }
+}
+
+setInterval(updateTestimonial,  3000)
+
+// updateTestimonial()
